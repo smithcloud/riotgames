05f836035ca35f27a1e14df7622743f76a105d04
Sungguk Hong <144807463+gmstcl@users.noreply.github.com>
2025-01-03 22:14:13 +0900

    Update SkillsBackendApplication.java

--- src/main/java/org/worldskills/cloud/skillsbackend/SkillsBackendApplication.java
+++ src/main/java/org/worldskills/cloud/skillsbackend/SkillsBackendApplication.java
@@ -47,7 +47,7 @@ public class SkillsBackendApplication {
     @GetMapping("/api/health")
     public ResponseEntity<Map<String, Object>> health() {
         Map<String, Object> data = new HashMap<>();
-        data.put("status", "GREEN");
+        data.put("status", "OK");
 
         log.info("GET | /api/health | 200");
 
